73b0a2bbde9fdbebf09c8dad8973fda73938b321
mohammedelnabawy <mohammedelnabawy11@gmail.com>
2023-05-26 18:41:05 +0300

    create route table

---
+++ terraform/network/aws_routing.tf
@@ -0,0 +1,28 @@
+resource "aws_route_table_association" "public-1" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public_table.id
+}
+
+resource "aws_route_table_association" "public-2" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public_table.id
+}
+
+resource "aws_route_table_association" "public-3" {
+  subnet_id      = aws_subnet.public-subnet-3.id
+  route_table_id = aws_route_table.public_table.id
+}
+
+resource "aws_route_table_association" "private-1" {
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.private_table.id
+}
+
+resource "aws_route_table_association" "private-2" {
+  subnet_id      = aws_subnet.private-subnet-2.id
+  route_table_id = aws_route_table.private_table.id
+}
+resource "aws_route_table_association" "private-3" {
+  subnet_id      = aws_subnet.private-subnet-3.id
+  route_table_id = aws_route_table.private_table.id
+}
