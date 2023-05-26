90e572c044de9ec71c226a65f55d69dc0d95dd07
mohammedelnabawy <mohammedelnabawy11@gmail.com>
2023-05-26 21:01:47 +0300

    network created

--- terraform/network/aws-subnet.tf
+++ terraform/network/aws-subnet.tf
@@ -18,16 +18,6 @@ resource "aws_subnet" "private-subnet-1" {
   }
 }
 
-resource "aws_subnet" "public-subnet-2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_2_cider
-  availability_zone = var.az_2
-map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet-2"
-  }
-}
-
 resource "aws_subnet" "private-subnet-2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_2_cider
@@ -35,16 +25,6 @@ resource "aws_subnet" "private-subnet-2" {
 
   tags = {
     Name = "private-subnet-2"
-  }
-}
-
-resource "aws_subnet" "public-subnet-3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_3_cider
-  availability_zone = var.az_3
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet-3"
   }
 }
 
--- terraform/network/aws_routing.tf
+++ terraform/network/aws_routing.tf
@@ -3,16 +3,6 @@ resource "aws_route_table_association" "public-1" {
   route_table_id = aws_route_table.public_table.id
 }
 
-resource "aws_route_table_association" "public-2" {
-  subnet_id      = aws_subnet.public-subnet-2.id
-  route_table_id = aws_route_table.public_table.id
-}
-
-resource "aws_route_table_association" "public-3" {
-  subnet_id      = aws_subnet.public-subnet-3.id
-  route_table_id = aws_route_table.public_table.id
-}
-
 resource "aws_route_table_association" "private-1" {
   subnet_id      = aws_subnet.private-subnet-1.id
   route_table_id = aws_route_table.private_table.id
